0d0f3a077888016aaae0b02ebecbc4f0d1dfe542
colby-smith <colbysmith99@hotmail.com>
2024-02-27 20:21:28 +0000

    Added check_length function

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
 def main():
     password_path = "passwords/password.txt"
     password = get_password(password_path)
+    check_length(password)
 
 def get_password(password_path):
     with open (password_path) as password:
         return password.read()
 
-def check_length(oassword):
-#8-32
+def check_length(password):
+    if len(password) > 32:
+        raise Exception ("Password cannot contain more than 32 characters")
+    if len(password) < 8:
+        raise Exception ("Password cannot contain less than 8 characters")
+    else:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 main()
