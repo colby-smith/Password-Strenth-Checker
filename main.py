8ba69409a06548fe2159756e3fd4b6dd7e9b63c8
colby-smith <colbysmith99@hotmail.com>
2024-02-27 21:33:54 +0000

    Added check_special_count function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
     password_path = "passwords/password.txt"
     password = get_password(password_path)
+    
     check_length(password)
     check_capitalisation(password)
+    check_digits(password)
+    check_special_count(password)
 
 def get_password(password_path):
     with open (password_path) as password:
@@ -13,8 +16,6 @@ def check_length(password):
         raise Exception ("Password cannot contain more than 32 characters")
     if len(password) < 8:
         raise Exception ("Password cannot contain less than 8 characters")
-    else:
-        return (None)
         
     
 def check_capitalisation(password):
@@ -23,8 +24,27 @@ def check_capitalisation(password):
         if char.isupper():
             uppercase_count += 1
     if uppercase_count >= 1:
-        return True
-        
+        print (f"your password contains {uppercase_count} capitalised characters")
+
+def check_digits(password):
+    digit_count = 0
+    for char in password:
+        if char.isdigit():
+            digit_count += 1
+    if digit_count >= 1:
+        print (f"your password contains {digit_count} numerial characters")
+
+def check_special_count(password):
+    special_characters = "!@#$%^&*()-+?_=,<>/"
+    special_count = 0
+    for char in password:
+        if char in special_characters:
+            special_count += 1
+    if special_count >= 1:
+        print(f"Your password contains {special_count} valid special characters")
+
+            
+    
 
 
 
